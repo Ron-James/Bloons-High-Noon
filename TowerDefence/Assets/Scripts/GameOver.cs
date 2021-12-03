c05d84e45a0a8ff97d5bb89638bc88344869adb6
JamesTheron36 <jajotheron@gmail.com>
2021-12-03 17:43:30 +0200

    fixed gameover problem

--- TowerDefence/Assets/Scripts/GameOver.cs
+++ TowerDefence/Assets/Scripts/GameOver.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollower : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
-    [SerializeField] Transform headPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = headPos.position;
+        
     }
 }
